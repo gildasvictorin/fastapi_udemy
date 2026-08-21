255eb537afa34bf217e803b81cfe2c5844e826c2
gildasvictorin <gildasvictorin@gmail.com>
2026-08-22 00:14:32 +0200

    new project of book

---
+++ books2.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+
+BOOKS = [
+    Book(1, "Computer Science Pro", "Codingwithrody", "A very nice book", 5),
+    Book(2, "Be fast with FastAPI", "Codingwithrody", "A great book", 5),
+    Book(3, "Muster Endpoints", "Codingwithrody", "A awesome book", 5),
+    Book(4, "HP1", "Author 1", "Book description", 2),
+    Book(5, "HP2", "Author 2", "Book description", 3),
+    Book(6, "HP3", "Author 3", "Book description", 1)
+]
+
+
+@app.get("/books")
+async def read_book():
+    return BOOKS
+
+
+
+
+@app.post("/create-book")
+async def create_book(book_request=Body()):
+    BOOKS.append(book_request)
